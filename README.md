d92bfe8b9f5e3f19e6f1f3dd3b88390550a41b19
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2022-05-28 00:51:26 +0500

    Добавит первый коммит

---
+++ README.md
@@ -0,0 +1,8 @@
+# Установка проекта
+```bash
+# Версия yarn без node_modules
+yarn set version stable
+
+# Установка проекта
+yarn install
+```
---
+++ src/components/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './app.less';
+
+const App = () => {
+  return (
+    <div className="app">
+      Hello, I'm AppComponent
+    </div>
+  );
+};
+
+export default App;
